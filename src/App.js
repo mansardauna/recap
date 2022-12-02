95277b1fc3d00130f4224ee65a5ed67fcfc46a4b
mansardauna <olaomansur@gmai.com>
2022-12-02 23:49:59 +0100

    usestate

--- src/App.js
+++ src/App.js
@@ -3,12 +3,31 @@ import './App.css';
 import './component/propTest'
 import PropTest from './component/propTest';
 import Testprops from './component/Testprops';
+import { useState } from 'react'
 
 const App = () => {
+  const [counter, setCounter] = useState(0)
+
+  // const handleClick = () => {
+  //   setCounter(counter + 1)
+
+
   return (
-    <Testprops />
+    <div>
+      <div>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>
+        plus
+      </button>
+
+      <button onClick={() => setCounter(0)}>
+        reset
+      </button>
+
+      <button onClick={() => setCounter(counter - 1)}>
+        minus
+      </button>
+    </div>
   )
 }
-
 
 export default App
