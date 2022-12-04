1832327de7f5a8acbf06d5b63c56b08d75d5c90a
mansardauna <olaomansur@gmai.com>
2022-12-04 13:25:17 +0100

    note

--- src/App.js
+++ src/App.js
@@ -2,41 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 import './component/propTest'
 import PropTest from './component/propTest';
-import Testprops from './component/Testprops';
+import Note from './component/Note';
 import { useState } from 'react'
 
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
 
-const App = (props) => {
-  const { course } = props
-  return (
-    <div>
-      <Course course={course} />
-    </div>
-  )
+  return <Note course={course} />
 }
-const Course = {
-  id: 1,
-  name: 'Half Stack application development',
-  parts: [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10,
-      id: 1
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7,
-      id: 2
-    },
-    {
-      name: 'State of a component',
-      exercises: 14,
-      id: 3
-    }
-  ]
-}
-
 
 export default App
-
 
